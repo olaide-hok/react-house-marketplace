d3eb6b366a6e743acfa4fd0e3dcdd18c223f7fba
olaide-hok <olaidekareem43@yahoo.com>
2022-02-20 10:50:10 +0100

    add pages files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import {Router, Routes, Route} from 'react-router-dom'
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import Signin from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
 
 function App() {
   return (
     <div>
-      <h1>My App</h1>
+      <Router>
+      <Routes>
+        <Route path='/' element={<Explore />} />
+        <Route path='/offers' element={<Offers />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />       
+      </Routes>
+
+      </Router>
+      
       
     </div>
   );
