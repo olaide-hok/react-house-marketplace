f7da38602bbaccb2edabbd6bea99c5da45963bbd
olaide-hok <olaidekareem43@yahoo.com>
2022-02-20 14:54:27 +0100

    add Sign in and Sign up pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Explore from './pages/Explore';
 import ForgotPassword from './pages/ForgotPassword';
 import Offers from './pages/Offers';
 import Profile from './pages/Profile';
-import Signin from './pages/SignIn';
+import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 
@@ -15,8 +15,8 @@ function App() {
         <Routes>
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/profile' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />       
         </Routes>
