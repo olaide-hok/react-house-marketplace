61c6def230b00021fc6aceb32458723368499595
olaide-hok <olaidekareem43@yahoo.com>
2022-02-22 13:14:24 +0100

    Start Create Listing Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,16 @@
 import {getAuth, updateProfile} from 'firebase/auth'
 import { useState } from 'react'
+import {Link} from 'react-router-dom'
 import {toast} from 'react-toastify'
 
 import { useNavigate } from 'react-router-dom'
 import {db} from '../firebase.config'
 import {updateDoc, doc} from 'firebase/firestore'
+
+import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
+import homeIcon from '../assets/svg/homeIcon.svg'
+
+
 
 function Profile() {
   const auth = getAuth()
@@ -97,6 +103,13 @@ function Profile() {
             />
           </form>
         </div>
+
+        <Link to='/create-listing' className='createListing'>
+          <img src={homeIcon} alt="home" />
+            <p>Sell or rent your home</p>
+            <img src={arrowRight} alt="arrow right" />
+        </Link>
+
       </main>
 
     </div>
