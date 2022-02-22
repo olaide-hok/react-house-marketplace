d66e36637e3751a8dc61daa3a4df39cd48093146
olaide-hok <olaidekareem43@yahoo.com>
2022-02-22 12:31:36 +0100

    Create Offers page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -41,7 +41,7 @@ function Navbar() {
                         ? 'navbarListItemNameActive' 
                         : 'navbarListItemName'}
                     >
-                        Offer
+                        Offers
                     </p>
                 </li>
                 <li className="navbarListItem" onClick={() => navigate('/profile')}>
